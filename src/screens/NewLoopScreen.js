97b945ea906301ff4bbdcb9ced3a2500a8c26aa2
Graham Carkner <gcarkner@gmail.com>
2021-03-11 16:56:34 -0500

    new loop screens

--- src/screens/NewLoopScreen.js
+++ src/screens/NewLoopScreen.js
@@ -2,7 +2,7 @@
 
 // React native and gui component imports
 import React, {Component} from 'react';
-import { View, Text, ScrollView, StyleSheet, StatusBar, Image, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
@@ -18,28 +18,66 @@ import {
 
 // Topbar
 import Topbar from '../components/Topbar';
+// import { styles } from '../styles/styles';
 
 class NewLoop extends Component {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+          loop_name: "",
+          loop_code: "123456",
+          done: false
+      };
     }
 
-    // back button onPress
-    toFeed = () => {
-        this.props.navigation.navigate("Home")
+    createLoop = () => {
+        // add create loop functionality
+        if(this.state.loop_name){
+            this.setState({done: true})
+        } else {
+            Alert.alert("Loop name cannot be blank!")
+        }
     }
     
     render() {
       return (
-        <>
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+
         <Topbar 
-            title="Create Loop" 
+            title={this.state.done ? "Loop Created!" : "Create Loop"} 
             center={true} 
             back={true}
             navigation={this.props.navigation}
         />
+
+        {this.state.done ?
+        <>
+            <Text style={styles.subhead}>Your Loop Code is:</Text>
+            <Text style={styles.loopCode}>{this.state.loop_code}</Text>
+            <Text style={styles.loopCodeSub}>Your friends can use this code to join the loop.</Text>
         </>
+        :
+        <>
+
+        <Text style={styles.subhead}>Name your loop:</Text>
+        <TextInput 
+            style={styles.loopNameInput}
+            onChangeText={text =>this.setState({loop_name: text})}
+            autoFocus
+            clearTextOnFocus
+        />
+
+        <TouchableOpacity 
+          style={styles.loopButton}
+          onPress={() => this.createLoop()}
+        >
+          <Text style={styles.buttonText}>Create Loop</Text>
+        </TouchableOpacity>
+
+        </>
+        }
+
+        </View>
       );
     }
   
@@ -48,22 +86,47 @@ class NewLoop extends Component {
 export default NewLoop;
 
 const styles = StyleSheet.create({
-  topBarBox: {
-      width: '100%',
-      backgroundColor: '#FF9999',
-  },
-  topBar: {
-      width: '90%',
-      alignSelf: 'center',
-      height: 60,
-      alignItems: "center",
-      justifyContent: 'center',
-      flexDirection: 'column',
-  },
-  feedTitle: {
-      fontSize: 25,
-      fontWeight: 'bold',
-      color: 'white',
-      fontStyle: 'italic',
-  }
+    subhead: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginTop: 10,
+        marginBottom: 5,
+        marginLeft: 20
+    },
+    loopNameInput: {
+        marginTop: 10,
+        marginHorizontal: 20,
+        height: 35,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: "#A6A6A6"
+    },
+    loopButton: {
+        width: '50%',
+        alignSelf: 'center',
+        marginBottom: 10,
+        paddingVertical: 5,
+        borderWidth: 0,
+        borderRadius: 50,
+        backgroundColor: '#FF9999',
+        borderWidth: 1,
+        marginTop: 30,
+    },
+    buttonText: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    loopCode: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 100
+    },
+    loopCodeSub: {
+        fontSize: 12,
+        textAlign: 'center',
+        marginTop: 5
+    }
 })
